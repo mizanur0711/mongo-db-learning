15e867af5099d4c460fe187169673b497d5e6b95
mizanur0711 <mizanur0711@gamil.com>
2022-12-20 19:04:52 +0600

    built in validation in mongoose,custom validation, Async validation scenario, schemaType Validation

--- index.js
+++ index.js
@@ -1,51 +1,94 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/playground')
-    .then(()=> console.log('Connected to MondoDB'))
-    .catch(err=>console.error('could not connect to MongoDb...',err));
+const mongoose = require("mongoose");
+mongoose
+  .connect("mongodb://localhost/playground")
+  .then(() => console.log("Connected to MondoDB"))
+  .catch((err) => console.error("could not connect to MongoDb...", err));
 
-    const courseSchema = new mongoose.Schema({
-        name:String,
-        author:String,
-        tags:[String],
-        date: {type:Date, default:Date.now},
-        isPublished: Boolean
-    });
-
-    const Course = mongoose.model('Course',courseSchema);
-    
-
-    async function createCourse(){
-        const course = new Course({
-            name:'Node.js course latest',
-            author: 'miz',
-            tags:['node','backend'],
-            isPublished:true
+const courseSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255,
+    // match: /regex/,
+  },
+  category: {
+    type: String,
+    required: true,
+    enum: ["web", "mobile", "network"],
+    lowecase: true,
+  },
+  author: String,
+  tags: {
+    type: Array,
+    validate: {
+      validator: function (v) {
+        return new Promise((resolve) => {
+          setTimeout(() => {
+            const result = v && v.length > 0;
+            resolve(result);
+          }, 4000);
         });
+      },
+      message: "A course should have at least one tag.",
+    },
+  },
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean,
+  price: {
+    type: Number,
+    required: function () {
+      return this.isPublished;
+    },
+    min: 10,
+    max: 200,
+    get: (v) => Math.round(v),
+    set: (v) => Math.round(v),
+  },
+});
 
-        const result = await course.save();
-        console.log(result);
-    }
+const Course = mongoose.model("Course", courseSchema);
 
-    //createCourse();
+async function createCourse() {
+  const course = new Course({
+    name: "Node.js course latest",
+    category: "web",
+    author: "miz",
+    tags: ["book"],
+    isPublished: true,
+    price: 15,
+  });
+  try {
+    const result = await course.save();
+    console.log(result);
+  } catch (ex) {
+    for (field in ex.errors) console.log(ex.errors[field].message);
+  }
+}
 
-    async function getCourse(){
-        const courses = await Course
-        .find({author: 'miz',isPublished: true})
-        .limit(2)
-        .sort({name: -1});
-        console.log(courses);
-    }
+createCourse();
 
-    getCourse();
+async function getCourse() {
+  const courses = await Course.find({ author: "miz", isPublished: true })
+    .limit(2)
+    .sort({ name: -1 });
+  console.log(courses);
+}
 
-    async function updateCourse(id){
-        const course = await Course.findByIdAndUpdate(id,{
-            $set:{
-                author: 'anon',
-                isPublished: false
-            }
-        },{new:true});
-        console.log(course);
-    }
+// getCourse();
 
-    updateCourse('63a053555335f7a7afcce5ed');
+async function updateCourse(id) {
+  const course = await Course.findByIdAndUpdate(
+    id,
+    {
+      $set: {
+        author: "anon",
+        isPublished: false,
+      },
+    },
+    { new: true }
+  );
+  console.log(course);
+}
+
+//updateCourse('63a053555335f7a7afcce5ed');
